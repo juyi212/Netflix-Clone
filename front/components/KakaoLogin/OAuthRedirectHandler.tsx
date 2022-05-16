9bec8df47d282af51b292ab4fea4483978d12e50
juyi212 <dea8307@naver.com>
2022-05-17 01:20:45 +0900

    feat: 카카오 로그인 토근 받아오기 성공

--- front/components/KakaoLogin/OAuthRedirectHandler.tsx
+++ front/components/KakaoLogin/OAuthRedirectHandler.tsx
@@ -6,16 +6,21 @@ const OAuthRedirectHandler = () => {
             let code = new URL(window.location.href).searchParams.get("code");
             let grant_type = "authorization_code";
             let client_id = "53fac96dcc45b034a080250c417a2227";
-            axios.post(`https://kauth.kakao.com/oauth/token?
-            grant_type=${grant_type}
-            &client_id=${client_id}
-            &redirect_uri=http://localhost:3000/oauth/callback/kakao
-            &code=${code}`
-            , {
-        headers: {
-            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            let data: any = {
+                grant_type: "authorization_code",
+                client_id: client_id,
+                redirect_uri: "http://localhost:3000/user/kakao",
+                code: code,
             }
-        })
+            const queryString = Object.keys(data)
+                .map((k:any)=> encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
+                .join('&');
+
+            axios.post('https://kauth.kakao.com/oauth/token', queryString, {
+                headers: {
+                  'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                }
+            })
             .then((res) => {
                 console.log(res)
             }).catch((error) => {
