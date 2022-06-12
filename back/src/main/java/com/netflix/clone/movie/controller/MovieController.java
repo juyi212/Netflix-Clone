10bf2db54edb9a5991756b0ca18301c87ba663da
zzdhxm12 <zzdhxm12@naver.com>
2022-06-13 00:57:55 +0900

    - ID별 영화 조회 Error 처리

--- back/src/main/java/com/netflix/clone/movie/controller/MovieController.java
+++ back/src/main/java/com/netflix/clone/movie/controller/MovieController.java
@@ -4,9 +4,13 @@ import com.netflix.clone.movie.service.MovieService;
 import com.netflix.clone.repository.dto.Movie;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/movie")
@@ -60,15 +64,32 @@ public class MovieController {
     /* 영화 ID 별 조회 */
     @ApiOperation(value = "영화 ID 별 조회 Restful API", response = Movie.class)
     @GetMapping("/movie_detail")
-    public Movie getMovieDetail(@RequestParam("movieId") String movieId) {
+    public ResponseEntity<?> getMovieDetail(@RequestParam("movieId") String movieId) {
+        HttpStatus status = null;
+        Map<String, Object> resultMap = new HashMap<>();
         Movie movie = null;
+
         try {
             movie = movieService.getMovieDetail(Integer.parseInt(movieId));
+            if(movie == null) {
+                resultMap.put("errMessage", "영화 정보가 존재하지 않습니다.");
+                resultMap.put("status", "error");
+                status = HttpStatus.OK;
+
+                return new ResponseEntity<Map<String, Object>>(resultMap, status);
+            }
+
+            resultMap.put("movie", movie);
+            status = HttpStatus.OK;
         } catch (Exception e) {
             e.printStackTrace();
+            resultMap.put("errMessage", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+            return new ResponseEntity<Map<String, Object>>(resultMap, status);
         }
 
-        return movie;
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
 
     /* 영화 좋아요 */
