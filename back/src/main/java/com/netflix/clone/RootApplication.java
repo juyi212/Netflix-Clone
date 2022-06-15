c6420ce08b3b7253ec431639c5ef05e826ff56ad
hobin <elwlahd555@naver.com>
2022-06-15 22:00:53 +0900

    2022-06-15 security fix

--- back/src/main/java/com/netflix/clone/RootApplication.java
+++ back/src/main/java/com/netflix/clone/RootApplication.java
@@ -22,7 +22,7 @@ public class RootApplication implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://3.39.105.32:3000")
 //		.allowedOrigins("http://3.39.105.32")
 		.allowedMethods("*")
 		.allowedHeaders("*")
