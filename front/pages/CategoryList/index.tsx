ed2d46d7680342236a66dc643c16316dabf610a0
juyi212 <dea8307@naver.com>
2022-07-02 00:38:26 +0900

    feat: 카테고리관 장르 swiper 만드는 중

--- front/pages/CategoryList/index.tsx
+++ front/pages/CategoryList/index.tsx
@@ -1,23 +1,48 @@
+import category from '@utils/category';
 import React, { useCallback, useContext, useEffect } from 'react';
+import { Category, Container } from './styles';
 
 
+const categoryList = [
+    {id: "12", name: "모험"},
+    {id: "14", name: "판타지"},
+    {id: "16", name: "애니메이션"},
+    {id: "18", name: "드라마"},
+    {id: "27", name: "공포"},
+    {id: "35", name: "역사"},
+    {id: "37", name: "서부"},
+    {id: "53", name: "스릴러"},
+    {id: "80", name: "범죄"},
+    {id: "99", name: "다큐멘터리"},
+    {id: "878", name: "SF"},
+    {id: "9648", name: "미스터리"},
+    {id: "10402", name: "음악"},
+    {id: "10749", name: "로맨스"},
+    {id: "10751", name: "가족"},
+    {id: "0752", name: "전쟁"},
+    {id: "10770", name: "TV 영화"},
+]
 
 
 const CategoryList = React.memo(() => {
 
-    
     return (
-        <div style={{ marginTop : "150px"}}>
-            <div>
-                <span>카테고리 리스트들 나열</span>
-            </div>
+        <Container>
+            <h1> 장르 </h1>
+            <div style={{ color: 'white'}}>
+                {categoryList.map((category: any) => {
+                    return (
+                        <Category>{category.name}</Category>
+                    )
+                })}
+            </div>          
             <div>
                 <span>국가 리스트들 나열</span>
             </div>
             <div>
                 기본 데이터들 보여주기 
             </div>
-        </div>
+        </Container>
     )})
 
 export default CategoryList;
---
+++ front/pages/CategoryList/styles.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+
+
+export const Container = styled.div`
+    margin: 130px 30px 0 30px;
+
+`
+
+export const Category = styled.span`
+    background-color: rgb(46, 46, 46);
+    padding: 0.917rem 2.917rem 1rem;
+    min-height: 3.677rem;
+    font-size: 1.25rem;
+    border-radius: 55px;
+    line-height: 1.4;
+`
