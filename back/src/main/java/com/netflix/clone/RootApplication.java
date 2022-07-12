4f4aedaaad6265617d2b549c2b3397b2c0d36864
hobin <66417732+elwlahd555@users.noreply.github.com>
2022-07-12 22:13:10 +0900

    Update RootApplication.java

--- back/src/main/java/com/netflix/clone/RootApplication.java
+++ back/src/main/java/com/netflix/clone/RootApplication.java
@@ -22,8 +22,8 @@ public class RootApplication implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-// 		.allowedOrigins("http://localhost:3000")
-		.allowedOrigins("http://3.39.105.32:3000")
+		.allowedOrigins("http://localhost:3000")
+// 		.allowedOrigins("http://3.39.105.32:3000")
 		.allowedMethods("*")
 		.allowedHeaders("*")
 		.exposedHeaders("auth-token").allowCredentials(true);
