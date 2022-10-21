c464ef306eec80116d4244998eb4a27072bdd871
juyi212 <dea8307@naver.com>
2022-10-21 16:49:33 +0900

    불필요한 코드 제거

--- front/hooks/kakaoUserInfo.ts
+++ front/hooks/kakaoUserInfo.ts
@@ -13,18 +13,6 @@ const kakaoUserInfo = async (access_token? : string): Promise<[]> => {
     let data = await window.Kakao.API.request({
         url: "/v2/user/me",
       });
-    console.log(data)
-    // axios.get("https://kapi.kakao.com/v2/user/me", 
-    // {
-    //     headers: {
-    //     'Authorization': `Bearer ${access_token}`,
-    //     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-    //     }
-    // }).then((res) => {
-    //     console.log(res.data)
-    // }).catch((err) => {
-    //     console.log(err)
-    // })
     return []
 }
 
